d8535a0b34eb881c0da3d7291b910fee0731eb0f
daxlar <azhang100@gmail.com>
2020-07-12 22:29:09 -0500

    proper hello world

--- main/main.c
+++ main/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
 
+const char TAG[] = "app_main";
 
 void app_main(void){
-    printf("hello world");
+    const TickType_t xDelay10ms = pdMS_TO_TICKS(1000);
+    while(1){
+        vTaskDelay(xDelay10ms);
+        ESP_LOGI(TAG, "hello world");
+    }
 }
